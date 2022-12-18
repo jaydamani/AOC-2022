9d6a2c2ff16ae5694de1ed4785a3c4fe5ffbe0eb
jaydamani <jaydamani567@gmail.com>
2022-12-18 16:16:56 +0530

    fix: update paths and ENV names

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import "module-alias/register"
 import { formatDay } from '@utils';
 import { readFileSync } from "fs"
 
-const day = process.env.npm_config_day ? parseInt(process.env.npm_config_day) : (new Date()).getDate()
-const part = process.env.npm_config_part ? parseInt(process.env.npm_config_part) : 1
+const day = process.env.day ? parseInt(process.env.day) : (new Date()).getDate()
+const part = process.env.part ? parseInt(process.env.part) : 1
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const run = require(`./day${formatDay(day)}.js`).default as (input: string, part: number) => string
-const input = readFileSync(`@input/day${formatDay(day)}`).toString()
+const run = require(`./day${formatDay(day)}`).default as (input: string, part: number) => string
+const input = readFileSync(`./input/day${formatDay(day)}.txt`).toString()
 
 console.log(`Day ${day} | Part ${part} - Solution: ${run(input, part)}`);
 

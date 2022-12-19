8244e0eecf0f12cd7f189329abb0d8656e644379
jaydamani <jaydamani567@gmail.com>
2022-12-19 23:13:48 +0530

    feat: soluntion for day 2

---
+++ src/day02/index.ts
@@ -0,0 +1,20 @@
+type col1 = "A" | "B" | "C";
+type col2 = "X" | "Y" | "Z";
+const index = { A: 1, B: 2, C: 3, X: 1, Y: 2, Z: 3 };
+
+export default function run(str: string, part: number) {
+  let arr = str.split("\n");
+  return arr.map(funcs[part - 1]).reduce((a, b) => a + b, 0).toString();
+}
+const funcs = [
+  function calPoints(col: string) {
+    let round = col.split(" ") as (col1 | col2)[];
+    let [a1, a2] = round.map((a) => index[a]);
+    return a2 + ((a2 - a1 + 4) % 3) * 3;
+  },
+  function calPoints2(col: string) {
+    let round = col.split(" ") as (col1 | col2)[];
+    let [a1, a2] = round.map((a) => index[a]);
+    return ((a1 + a2) % 3) - 2 + a2 * 3;
+  },
+];

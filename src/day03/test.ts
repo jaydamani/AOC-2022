02ea29360ad9c4c32fb0d1abb145b1b3b4da15ad
jaydamani <jaydamani567@gmail.com>
2022-12-22 21:15:35 +0530

    feat: solution for day 3

---
+++ src/day03/test.ts
@@ -0,0 +1,7 @@
+export const answers = [157, 70]
+export const input = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`

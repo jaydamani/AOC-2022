f622436cba3c81712a74faf62724a3d41cd5f9d3
jaydamani <jaydamani567@gmail.com>
2022-12-29 00:47:22 +0530

    Feat: solution for day 6

---
+++ src/day06/index.ts
@@ -0,0 +1,6 @@
+export default function(input: string, part: number){
+    const num = part == 1 ? 4 : 14
+    const arr = Array.from(input)
+    return arr.map((_,i, arr) => new Set(arr.slice(i, i + num)))
+    .findIndex(s => s.size == num) + num
+}
---
+++ src/day06/test.ts
@@ -0,0 +1,2 @@
+export const answers = [7]
+export const input = `mjqjpqmgbljsphdztnvjfqwrcgsmlb`
